347d173427a6fe9f20d3826f53d13c759d5014be
Szabó Gergő <lest12110@gmail.com>
2025-07-01 00:20:53 +0200

    setup

--- run.sh
+++ run.sh
@@ -2,4 +2,5 @@ rm -rf .venv
 python3 -m venv .venv
 source .venv/bin/activate
 pip3 install -r requirements.txt
+open http://127.0.0.1:4637/storage
 python3 backend.py
---
+++ setup.sh
@@ -0,0 +1,12 @@
+baseurl="https://raw.githubusercontent.com/CyberHorizonLtd/macstats/refs/heads/main/"
+files=(
+    "templates/pages/storage.html"
+    "backend.py"
+    "requirements.txt"
+    "run.sh"
+)
+
+for file in "${files[@]}"; do
+    echo "Downloading $file..."
+    curl -O "${baseurl}${file}"
+done
