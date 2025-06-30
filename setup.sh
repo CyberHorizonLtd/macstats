242a810dd86cc616f7a63f045a23965bf7805fb9
Szabó Gergő <lest12110@gmail.com>
2025-07-01 00:43:40 +0200

    cleanup

--- setup.sh
+++ setup.sh
@@ -6,10 +6,28 @@ files=(
     "requirements.txt"
     "run.sh"
 )
+
+#remove files
+for file in "${files[@]}"; do
+    if [ -f "$file" ]; then
+        echo "Removing $file..."
+        rm "$file"
+    fi
+done
+
 templateextension="templates/pages/"
 templates=(
     "storage.html"
 )
+
+#remove templates
+for template in "${templates[@]}"; do
+    if [ -f "${templateextension}${template}" ]; then
+        echo "Removing ${templateextension}${template}..."
+        rm "${templateextension}${template}"
+    fi
+done
+
 for file in "${files[@]}"; do
     echo "Downloading $file..."
     curl -O "${baseurl}${file}"
